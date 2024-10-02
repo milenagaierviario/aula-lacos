af02f0fbc9769802d582f142cfd681fff29f346f
Milena <mgaier234@gmail.com>
2024-10-02 11:57:38 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,33 @@
+/*let calculos = Number(prompt("Digite um número ou 0 para sair:")) ;
+let soma = 0
+
+while (calculos !== 0) {
+  calculos = Number(prompt("Digite um número ou 0 para sair:"))
+  soma = soma + calculos
+}
+console.log(soma)
+
+  let numeros = [30, 60 , 10 , 5 , 15]
+
+
+  function calcula(array) {
+    let maior = 0
+    for(let m = 0 ; m < array.lenght ; m++ ) {
+      if(array[m] . maior) {
+        maior = array[i]
+      }
+    return(maior)
+  }
+  }
+  console.log(calcula(numeros))*/
+
+  const palavras = [ "Oi" , "sumido" , "tudo" , "bem?" , "Saudades"]
+  const fraseC = 
+   function frase(array) {
+  
+    for(let palavra of array) {
+      console.log(palavra) 
+      let fraseEscrita = ("Oi sumido tudo bem? Saudades")
+    }
+   }
+console.log(frase(palavras))
